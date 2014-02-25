ffece44ba8b9c6be00a6b680cca76ac88393c65d
Kamal <kkjoshi.it@gmail.com>
2014-02-25 17:34:50 +0530

    adding test for threader and anti-abuse modules

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
 var core = require("../test/mock-core.js")();
 var x = require("../validator/validator-test.js");
 var assert = require("assert");
+require("../threader/threader-test.js");
+require("../anti-abuse/anti-abuse-test.js");
